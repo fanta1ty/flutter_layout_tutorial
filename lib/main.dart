9bd6a9a3e742afda2a02470be1412fafe123d96a
Thinh Nguyen <thinhnguyen12389@gmail.com>
2024-05-20 16:31:35 +0700

    feat: [TCH-07] Added the text section

--- lib/main.dart
+++ lib/main.dart
@@ -148,3 +148,23 @@ class ButtonWithText extends StatelessWidget {
     );
   }
 }
+
+class TextSection extends StatelessWidget {
+  final String description;
+
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+  }
+}
